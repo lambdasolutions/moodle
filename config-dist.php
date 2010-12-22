3265557867a45924dba9a76de9d48cd414b47891
Jonathan Harker <jonathan@catalyst.net.nz>
2010-12-22 16:19:43 +1300

    config-dist: Add default aspell and du paths.

--- config-dist.php
+++ config-dist.php
@@ -31,6 +31,9 @@ unset($CFG);  // Ignore this line
 global $CFG;  // This is necessary here for PHPUnit execution
 $CFG = new stdClass();
 
+$CFG->aspellpath  = '/usr/bin/aspell';
+$CFG->pathtodu    = '/usr/bin/du';
+
 //=========================================================================
 // 1. DATABASE SETUP
 //=========================================================================
