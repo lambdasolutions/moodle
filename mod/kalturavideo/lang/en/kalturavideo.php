dc860ab3cae4d84cd754d956e89b4b681245e952
Brett Wilkins <brett@catalyst.net.nz>
2011-04-08 07:43:45 +1200

    mod/kalturavideo: andding lang strings

--- mod/kalturavideo/lang/en/kalturavideo.php
+++ mod/kalturavideo/lang/en/kalturavideo.php
@@ -7,21 +7,30 @@ $string['configframesize'] = 'configframesize**placeholder';
 $string['configsecretphrase'] = 'configsecretphrase**placeholder';
 $string['rolesinparams'] = 'rolesinparams**placeholder';
 $string['configrolesinparams'] = 'configrolesinparams**placeholder';
-$string['displayoptions'] = 'displayoptions**placeholder';
-$string['configdisplayoptions'] = 'configdisplayoptions**placeholder';
-$string['printheading'] = 'printheading**placeholder';
-$string['printheadingexplain'] = 'printheadingexplain**placeholder';
-$string['printintro'] = 'printintro**placeholder';
-$string['printintroexplain'] = 'printintroexplain**placeholder';
-$string['displayselect'] = 'displayselect**placeholder';
-$string['displayselectexplain'] = 'displayselectexplain**placeholder';
-$string['popupwidth'] = 'popupwidth**placeholder';
-$string['popupwidthexplain'] = 'popupwidthexplain**placeholder';
-$string['popupheight'] = 'popupheight**placeholder';
-$string['popupheightexplain'] = 'popupheightexplain**placeholder';
-$string['contentheader'] = 'contentheader**placeholder';
+$string['displayoptions'] = 'Display options';
+$string['configdisplayoptions'] = 'Configure display options';
+$string['printheading'] = 'Display Heading';
+$string['printheadingexplain'] = 'Display heading explanation';
+$string['printintro'] = 'Display description';
+$string['printintroexplain'] = 'Display description explanation';
+$string['displayselect'] = 'Display';
+$string['displayselectexplain'] = 'Explain display...';
+$string['displayselect_help'] = 'This setting, together with the URL file type and whether the browser allows embedding, determines how the URL is displayed. Options may include:
+
+* Automatic - The best display option for the URL is selected automatically
+* Embed - The URL is displayed within the page below the navigation bar together with the URL description and any blocks
+* Force download - The user is prompted to download the URL file
+* Open - Only the URL is displayed in the browser window
+* In pop-up - The URL is displayed in a new browser window without menus or an address bar
+* In frame - The URL is displayed within a frame below the the navigation bar and URL description
+* New window - The URL is displayed in a new browser window with menus and an address bar';
+$string['popupwidth'] = 'Popup width';
+$string['popupwidthexplain'] = 'Popup width explanation';
+$string['popupheight'] = 'Popup height';
+$string['popupheightexplain'] = 'Popup height explanation';
+$string['contentheader'] = 'Video Content';
 $string['kalturaentry'] = 'Kaltura Entry';
-$string['optionsheader'] = 'optionsheader**placeholder';
+$string['optionsheader'] = 'Display Options';
 $string['replacevideo'] = 'Replace Video';
 $string['pluginadministration'] = 'Kaltura Video Admin';
 ?>
