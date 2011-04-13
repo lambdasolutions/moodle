a750ffc3983445f1eeca0284e26f09d92be8f690
Brett Wilkins <brett@catalyst.net.nz>
2011-04-14 09:33:36 +1200

    local/kaltura: cleaning out everything we don't need

--- local/kaltura/js/video.assignment.js
+++
@@ -1,7 +0,0 @@
-function do_on_wait() {
-    var url = main.params['wwwroot'] + "/local/kaltura/keditor.php?entry_id=" + document.getElementById("id_widget").value;
-
-    document.getElementById("btn_remix").style.display = "inline";
-    document.getElementById("btn_addvid").value = main.params["replacesubmission"];
-    document.getElementById("btn_remix").onclick = new Function("kalturaInitModalBox('" + url + "', {width:890, height:546})");
-}
