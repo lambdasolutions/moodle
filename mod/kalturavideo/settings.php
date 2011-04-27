931daab2406af7f98b6ebe721336e396078d2cbb
Brett Wilkins <brett@catalyst.net.nz>
2011-04-27 13:31:24 +1200

    mod/kalturavideo/settings: clearing some PHP warnings

--- mod/kalturavideo/settings.php
+++ mod/kalturavideo/settings.php
@@ -29,30 +29,24 @@ defined('MOODLE_INTERNAL') || die;
 if ($ADMIN->fulltree) {
     require_once("$CFG->libdir/resourcelib.php");
 
-    $displayoptions = resourcelib_get_displayoptions(array(RESOURCELIB_DISPLAY_AUTO,
-                                                           RESOURCELIB_DISPLAY_EMBED,
-                                                           RESOURCELIB_DISPLAY_NEW,
-                                                           RESOURCELIB_DISPLAY_POPUP,
+/*    $displayoptions = resourcelib_get_displayoptions(array(RESOURCELIB_DISPLAY_AUTO,
                                                           ));
     $defaultdisplayoptions = array(RESOURCELIB_DISPLAY_AUTO,
-                                   RESOURCELIB_DISPLAY_EMBED,
-                                   RESOURCELIB_DISPLAY_NEW,
-                                   RESOURCELIB_DISPLAY_POPUP,
-                                  );
+                                  );*/
 
     //--- general settings -----------------------------------------------------------------------------------
     $settings->add(new admin_setting_configcheckbox('kalturavideo/requiremodintro',
         get_string('requiremodintro', 'admin'), get_string('configrequiremodintro', 'admin'), 0));
-    $settings->add(new admin_setting_configmultiselect('kalturavideo/displayoptions',
+/*    $settings->add(new admin_setting_configmultiselect('kalturavideo/displayoptions',
         get_string('displayoptions', 'kalturavideo'), get_string('configdisplayoptions', 'kalturavideo'),
-        $defaultdisplayoptions, $displayoptions));
+        $defaultdisplayoptions, $displayoptions));*/
     $settings->add(new admin_setting_configselect('kalturavideo/player_theme',
         get_string('playertheme','kalturavideo'), get_string('playerthemeexplain','kalturavideo'),
-        array('value'=>'light'),
+        'light',
         array('light'=> get_string('light', 'kalturavideo'), 'dark'=>get_string('dark','kalturavideo'))));
     $settings->add(new admin_setting_configselect('kalturavideo/editor_theme',
         get_string('editortheme','kalturavideo'), get_string('editorthemeexplain','kalturavideo'),
-        array('value'=>'light'),
+        'light',
         array('light'=> get_string('light', 'kalturavideo'), 'dark'=>get_string('dark','kalturavideo'))));
 
     //--- modedit defaults -----------------------------------------------------------------------------------
@@ -64,13 +58,13 @@ if ($ADMIN->fulltree) {
     $settings->add(new admin_setting_configcheckbox_with_advanced('kalturavideo/printintro',
         get_string('printintro', 'kalturavideo'), get_string('printintroexplain', 'kalturavideo'),
         array('value'=>1, 'adv'=>false)));
-    $settings->add(new admin_setting_configselect_with_advanced('kalturavideo/display',
+/*    $settings->add(new admin_setting_configselect_with_advanced('kalturavideo/display',
         get_string('displayselect', 'kalturavideo'), get_string('displayselectexplain', 'kalturavideo'),
-        array('value'=>RESOURCELIB_DISPLAY_AUTO, 'adv'=>false), $displayoptions));
-    $settings->add(new admin_setting_configtext_with_advanced('kalturavideo/popupwidth',
+        array('value'=>RESOURCELIB_DISPLAY_AUTO, 'adv'=>false), $displayoptions));*/
+/*    $settings->add(new admin_setting_configtext_with_advanced('kalturavideo/popupwidth',
         get_string('popupwidth', 'kalturavideo'), get_string('popupwidthexplain', 'kalturavideo'),
         array('value'=>620, 'adv'=>true), PARAM_INT, 7));
     $settings->add(new admin_setting_configtext_with_advanced('kalturavideo/popupheight',
         get_string('popupheight', 'kalturavideo'), get_string('popupheightexplain', 'kalturavideo'),
-        array('value'=>450, 'adv'=>true), PARAM_INT, 7));
+        array('value'=>450, 'adv'=>true), PARAM_INT, 7));*/
 }
