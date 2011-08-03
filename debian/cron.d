b64835bfefc996f20b80b14de9a2e9629d0419e8
Brett Wilkins <brett@catalyst.net.nz>
2011-08-03 14:45:51 +1200

    removing debian packaging after merge

--- debian/cron.d
+++
@@ -1,4 +0,0 @@
-#
-# Cron job for the 'feature-kaltura' Moodle instance
-#
-4-59/5 * * * * www-data /usr/bin/site-crondispatcher /var/www/feature-kaltura/moodle/admin/cli/cron.php moodle-site-feature-kaltura 28
--- dhmm_version.php
+++
@@ -1,5 +0,0 @@
-<?
-    /*
-     *  moodle-site-feature-kaltura was created using dh-make-moodle 0.23 
-     */
-?>
