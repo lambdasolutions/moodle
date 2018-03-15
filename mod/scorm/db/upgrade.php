07cc0241cb5d3c452279a1339e0c10c272448965
Dan Marsden <dan@danmarsden.com>
2018-03-15 13:28:07 +1300

    MDL-59857 mod_scorm: use valid length for grade type field.

--- mod/scorm/db/upgrade.php
+++ mod/scorm/db/upgrade.php
@@ -123,5 +123,18 @@ function xmldb_scorm_upgrade($oldversion) {
     // Automatically generated Moodle v3.2.0 release upgrade line.
     // Put any upgrade step following this.
 
+    if ($oldversion < 2016120502) {
+
+        // Changing precision of field completionscorerequired on table scorm to (10).
+        $table = new xmldb_table('scorm');
+        $field = new xmldb_field('completionscorerequired', XMLDB_TYPE_INTEGER, '10', null, null, null, null, 'completionstatusrequired');
+
+        // Launch change of precision for field completionscorerequired.
+        $dbman->change_field_precision($table, $field);
+
+        // Scorm savepoint reached.
+        upgrade_mod_savepoint(true, 2016120502, 'scorm');
+    }
+
     return true;
 }
